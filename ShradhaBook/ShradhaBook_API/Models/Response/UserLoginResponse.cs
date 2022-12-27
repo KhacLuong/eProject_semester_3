e8c32e09428638786eca795887bd5fd811b76f0b
yotsubasuzu <nguyenlinh125891@gmail.com>
2022-12-27 10:25:59 +0700

    login response with accessToken and refreshToken

--- ShradhaBook/ShradhaBook_API/Models/Response/UserLoginResponse.cs
+++ ShradhaBook/ShradhaBook_API/Models/Response/UserLoginResponse.cs
@@ -2,8 +2,6 @@
 {
     public class UserLoginResponse
     {
-        public string? Name { get; set; }
-        public string Email { get; set; } = string.Empty;
         public string AccessToken { get; set; } = string.Empty;
         public string RefreshToken { get; set; } = string.Empty;
     }
