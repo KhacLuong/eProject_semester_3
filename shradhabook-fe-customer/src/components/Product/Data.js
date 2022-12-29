cd803516d6305b1bc4504218f27596db26d2cfd1
Duc Anh <ducanhlekha2802@gmail.com>
2022-12-29 08:59:30 +0700

    feature/product_detail_page

--- shradhabook-fe-customer/src/components/Product/Data.js
+++ shradhabook-fe-customer/src/components/Product/Data.js
@@ -1,6 +1,25 @@
 import book1 from "../../assets/image/books/book_1.png";
 import book2 from "../../assets/image/books/book2.png";
 import book3 from "../../assets/image/books/book3.png";
+import book4 from "../../assets/image/books/book4.png";
+import book5 from "../../assets/image/books/book5.png";
+import book6 from "../../assets/image/books/book6.png";
+import book7 from "../../assets/image/books/book7.png";
+import book8 from "../../assets/image/books/book8.png";
+import book9 from "../../assets/image/books/book9.png";
+import book10 from "../../assets/image/books/book10.png";
+import book11 from "../../assets/image/books/book11.png";
+import book12 from "../../assets/image/books/book12.png";
+import book13 from "../../assets/image/books/book13.png";
+import book14 from "../../assets/image/books/book15.png";
+import book15 from "../../assets/image/books/book15.png";
+import book16 from "../../assets/image/books/book16.png";
+import book17 from "../../assets/image/books/book17.png";
+import book18 from "../../assets/image/books/book18.png";
+import book19 from "../../assets/image/books/book19.png";
+import book20 from "../../assets/image/books/book20.png";
+import book21 from "../../assets/image/books/book21.png";
+import book22 from "../../assets/image/books/book22.png";
 
 export const Data = {
     "EC": 0,
@@ -34,7 +53,7 @@ export const Data = {
             },
             {
                 id: 4,
-                imageProductName: book1,
+                imageProductName: book4,
                 name: 'Blood on the Snow',
                 author: 'Rex Rios',
                 price: '216.99',
@@ -42,7 +61,7 @@ export const Data = {
             },
             {
                 id: 5,
-                imageProductName: book2,
+                imageProductName: book5,
                 name: 'Blood on the Snow',
                 author: 'Rex Rios',
                 price: '116.99',
@@ -50,7 +69,7 @@ export const Data = {
             },
             {
                 id: 6,
-                imageProductName: book3,
+                imageProductName: book6,
                 name: 'Blood on the Snow',
                 author: 'Rex Rios',
                 price: '116.99',
@@ -58,7 +77,7 @@ export const Data = {
             },
             {
                 id: 7,
-                imageProductName: book1,
+                imageProductName: book7,
                 name: 'Blood on the Snow',
                 author: 'Rex Rios',
                 price: '216.99',
@@ -66,7 +85,7 @@ export const Data = {
             },
             {
                 id: 8,
-                imageProductName: book2,
+                imageProductName: book8,
                 name: 'Blood on the Snow',
                 author: 'Rex Rios',
                 price: '116.99',
@@ -74,7 +93,7 @@ export const Data = {
             },
             {
                 id: 9,
-                imageProductName: book3,
+                imageProductName: book9,
                 name: 'Blood on the Snow',
                 author: 'Rex Rios',
                 price: '116.99',
@@ -82,7 +101,7 @@ export const Data = {
             },
             {
                 id: 10,
-                imageProductName: book1,
+                imageProductName: book10,
                 name: 'Blood on the Snow',
                 author: 'Rex Rios',
                 price: '216.99',
@@ -90,7 +109,7 @@ export const Data = {
             },
             {
                 id: 11,
-                imageProductName: book2,
+                imageProductName: book11,
                 name: 'Blood on the Snow',
                 author: 'Rex Rios',
                 price: '116.99',
@@ -98,7 +117,7 @@ export const Data = {
             },
             {
                 id: 12,
-                imageProductName: book3,
+                imageProductName: book12,
                 name: 'Blood on the Snow',
                 author: 'Rex Rios',
                 price: '116.99',
@@ -106,12 +125,85 @@ export const Data = {
             },
             {
                 id: 13,
-                imageProductName: book1,
+                imageProductName: book13,
                 name: 'Blood on the Snow',
                 author: 'Rex Rios',
                 price: '116.99',
                 slug: 'blood-on-the-snow'
-            }
+            },
+            {
+                id: 14,
+                imageProductName: book14,
+                name: 'Blood on the Snow',
+                author: 'Rex Rios',
+                price: '116.99',
+                slug: 'blood-on-the-snow'
+            },
+            {
+                id: 15,
+                imageProductName: book15,
+                name: 'Blood on the Snow',
+                author: 'Rex Rios',
+                price: '116.99',
+                slug: 'blood-on-the-snow'
+            },
+            {
+                id: 16,
+                imageProductName: book16,
+                name: 'Blood on the Snow',
+                author: 'Rex Rios',
+                price: '116.99',
+                slug: 'blood-on-the-snow'
+            },
+            {
+                id: 17,
+                imageProductName: book17,
+                name: 'Blood on the Snow saksd ioasdjoasdiosj',
+                author: 'Rex Rios',
+                price: '116.99',
+                slug: 'blood-on-the-snow'
+            },
+            {
+                id: 18,
+                imageProductName: book18,
+                name: 'Blood on the Snow',
+                author: 'Rex Rios',
+                price: '116.99',
+                slug: 'blood-on-the-snow'
+            },
+            {
+                id: 19,
+                imageProductName: book19,
+                name: 'Blood on the Snow',
+                author: 'Rex Rios',
+                price: '116.99',
+                slug: 'blood-on-the-snow'
+            },
+            {
+                id: 20,
+                imageProductName: book20,
+                name: 'Blood on the Snow',
+                author: 'Rex Rios',
+                price: '116.99',
+                slug: 'blood-on-the-snow'
+            },
+            {
+                id: 21,
+                imageProductName: book21,
+                name: 'Blood on the Snow',
+                author: 'Rex Rios',
+                price: '116.99',
+                slug: 'blood-on-the-snow'
+            },
+            {
+                id: 22,
+                imageProductName: book22,
+                name: 'Blood on the Snow',
+                author: 'Rex Rios',
+                price: '116.99',
+                slug: 'blood-on-the-snow'
+            },
+
         ]
     }
 }
--- shradhabook-fe-customer/src/redux/action/userAction.js
+++ shradhabook-fe-customer/src/redux/action/userAction.js
@@ -1,6 +1,6 @@
 export const FETCH_USER_LOGIN_SUCCESS = 'FETCH_USER_LOGIN_SUCCESS'
 export const FETCH_USER_LOGOUT_SUCCESS = 'FETCH_USER_LOGOUT_SUCCESS'
-
+export const REFRESH_USER_TOKEN = 'REFRESH_USER_TOKEN'
 export const doLogin = (data) => {
     return {
         type: FETCH_USER_LOGIN_SUCCESS,
@@ -11,5 +11,12 @@ export const doLogin = (data) => {
 export const doLogout = () => {
     return {
         type: FETCH_USER_LOGOUT_SUCCESS,
+    }
+}
+
+export const doRefreshToken = (data) => {
+    return {
+        type: REFRESH_USER_TOKEN,
+        payload: data
     }
 }
